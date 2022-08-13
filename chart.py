fba2042307d9069a086dac485ca2714ed88039e3
Pedro <pedroalmeidavrb@gmail.com>
2022-08-12 22:54:11 -0300

    Tentando implementar saida em csv. Implementando codigo em py para gerar graficos

---
+++ chart.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+x = []
+y = []
+
+#dataset = open('dataset.csv', 'r')
+
+plt.plot([1,2,3], [4,5,6])
+
+plt.title('Vazão da rede sem perdas')
+plt.xlabel('Nome do eixo X')
+plt.ylabel("Nome do eixo Y")
+
+plt.show()
+
+#plt.title('Vazão da rede com perdas')
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Buffer } from "node:buffer";
 import { SafeUdpServer } from "./SafeUdpServer.js";
 import { SafeUdpReceiver } from "./SafeUdpReceiver.js";
 import path from "path";
+import { stringify } from 'csv-stringify';
 
 const { SIZE_OF_BUFFER = 1024, PORT = 2030, CLIENT_PORT = 30 } = process.env;
 
@@ -17,4 +18,17 @@ const client = new SafeUdpReceiver({
 
 const image = fs.readFileSync("src/images/image2.jpg");
 
+// Testando a saída em arquivo
+
+/* fs.writeFile('saida.csv', "pedro", {encoding: 'utf-8', flag: 'w+'}, function(err) {
+  if(err)  
+    console.log("Arquivo n pode ser gravado")
+})  */
+
 server.sendFile(image, CLIENT_PORT);
+
+
+function testando ()
+{
+  console.log("Teste")
+}
