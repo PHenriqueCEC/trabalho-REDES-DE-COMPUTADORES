9de30d08fdd4936c077466d309bc0c57968d87b6
Matheus <werneckmatheus12@gmail.com>
2022-08-13 16:11:49 -0300

    Implementado handshake

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import { SafeUdpServer } from "./SafeUdpServer.js";
 import { SafeUdpReceiver } from "./SafeUdpReceiver.js";
 import path from "path";
-import { stringify } from 'csv-stringify';
+import { stringify } from "csv-stringify";
 
 const { SIZE_OF_BUFFER = 1024, PORT = 2030, CLIENT_PORT = 30 } = process.env;
 
@@ -25,9 +25,3 @@ const image = fs.readFileSync("src/images/image2.jpg");
 })  */
 
 server.sendFile(image, CLIENT_PORT);
-
-
-function testando ()
-{
-  console.log("Teste")
-}
