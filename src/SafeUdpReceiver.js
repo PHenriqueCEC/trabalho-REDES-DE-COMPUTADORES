95ceb8937a9f1179eea614b315f678ad768e8af8
Matheus Werneck <werneckmatheus12@gmail.com>
2022-08-15 13:35:06 -0300

    feat: implementado remontagem de pacotes e des e desconexão

--- src/SafeUdpReceiver.js
+++ src/SafeUdpReceiver.js
@@ -1,17 +1,18 @@
-import { Console } from "console";
 import dgram from "dgram";
+import fs from "fs";
 import logger from "./utils/logger.js";
-
-const serverUrl = "127.0.0.1";
+import { PACKAGE_TYPE, PACKAGE_TYPE_DICTIONARY } from "./constants/index.js";
 
 export class SafeUdpReceiver {
   constructor({ serverUrl, port, serverPort }) {
-    this.server = dgram.createSocket("udp4");
+    this.server = null;
+
     this.serverUrl = serverUrl;
     this.lastNumberOfSequence = 0;
     this.port = port;
     this.serverPort = serverPort;
     this.file = null;
+    this.receivedPackages = [];
 
     this.initServer();
   }
@@ -20,13 +21,12 @@ export class SafeUdpReceiver {
     return parseInt(Math.random() * 10) > 2;
   }
 
-  /*@todo: remontar o arquivo */
   /*@todo: controle de fluxo*/
 
-  getPackageType(data = new Buffer()) {
+  getPackageType(data) {
     const packageType = data.readInt8();
 
-    return packageType === 1 ? "connection" : "data";
+    return PACKAGE_TYPE_DICTIONARY[packageType];
   }
 
   initServer() {
@@ -36,19 +36,23 @@ export class SafeUdpReceiver {
     this.initOnListening();
     this.initOnMessage();
 
-    this.server.on("error", (err) => {});
+    this.server.on("error", (err) => {
+      logger.error(`Error in receiver ${err}`);
+    });
   }
 
   initOnMessage() {
     this.server.on("message", (msg) => {
+      logger.info(`Server got a message with legnth of: ${msg.length}`);
+
       const packageType = this.getPackageType(msg);
 
-      if (packageType === "connection") this.handleConnectionPackage(msg);
-      else {
-        this.handleDataPackage(msg);
-      }
+      console.log("Package type", packageType);
 
-      console.log(`Server got a message with legnth of: ${msg.length}`);
+      if (packageType === "connection") this.handleConnectionPackage(msg);
+      else if (packageType === "data") this.handleDataPackage(msg);
+      else if (packageType === "disconnection")
+        this.handleDisconectionPackage();
     });
   }
 
@@ -59,35 +63,50 @@ export class SafeUdpReceiver {
     });
   }
 
-  handleConnectionPackage(msg = new Buffer()) {
+  handleConnectionPackage(msg) {
     this.numberOfPackages = msg.readUInt32BE(1);
+    this.receivedPackages = new Array(this.numberOfPackages);
+
     const initialPackageSeqNum = msg.readInt32BE(5);
 
     const initialWindowSize = 10;
 
     const data = Buffer.alloc(9);
-
     //Escreve tipo de pacote
-    data.writeInt8(1);
+    data.writeInt8(PACKAGE_TYPE.connection);
     data.writeInt32BE(initialPackageSeqNum, 1);
     data.writeUInt32BE(initialWindowSize, 5);
 
     this.server.send(data, this.serverPort);
   }
 
+  handleDisconectionPackage() {
+    this.remountFile();
+  }
+
   handleDataPackage(msg) {
-    this.receivedSeqNum = msg.readUInt32BE(1);
+    const receivedSeqNum = msg.readUInt32BE(1);
 
     const data = msg.subarray(100);
+    this.receivedPackages[receivedSeqNum] = data;
 
-    const packageToSend = Buffer.alloc(5);
+    const packageToSend = this.makePackage(receivedSeqNum);
 
-    packageToSend.writeUInt8(2);
-    packageToSend.writeUint32BE(this.receivedSeqNum, 1);
-
-    //Usa uma função randomica para simular perda de pacotes
     if (this.acceptPackage()) this.server.send(packageToSend, this.serverPort);
   }
 
-  makeHeader() {}
+  makePackage(receivedSeqNum) {
+    const packageToSend = Buffer.alloc(5);
+
+    packageToSend.writeUInt8(2);
+    packageToSend.writeUint32BE(receivedSeqNum, 1);
+
+    return packageToSend;
+  }
+
+  remountFile() {
+    const file = Buffer.concat(this.receivedPackages);
+
+    fs.writeFileSync("generated.png", file);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import "dotenv/config";
 import fs from "fs";
 import { SafeUdpServer } from "./SafeUdpServer.js";
 import { SafeUdpReceiver } from "./SafeUdpReceiver.js";
-import path from "path";
-import { stringify } from "csv-stringify";
 
 const { SIZE_OF_BUFFER = 1024, PORT = 2500, CLIENT_PORT = 2500 } = process.env;
 
 const server = new SafeUdpServer({ bufferSize: SIZE_OF_BUFFER, port: PORT });
-const client = new SafeUdpReceiver({
+
+new SafeUdpReceiver({
   port: CLIENT_PORT,
   serverPort: PORT,
 });
 
-// client.send({ data: buff, serverUrl: PORT });
+const image = fs.readFileSync("src/images/10MBImage.jpg");
 
-const image = fs.readFileSync("src/images/image2.jpg");
+fs.unlinkSync("combined.log");
+fs.unlinkSync("error.log");
 
 server.sendFile(image, CLIENT_PORT);
