c098db8174d120c0327592708be88ab905cdecba
Pedro <pedroalmeidavrb@gmail.com>
2022-08-18 10:39:50 -0300

    Fazendo entrada por terminal

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import fs from "fs";
 import { SafeUdpServer } from "./SafeUdpServer.js";
 import { SafeUdpReceiver } from "./SafeUdpReceiver.js";
+import inquirer from "inquirer";
+//import readline from "readline";
 
 const { SIZE_OF_BUFFER = 1024, PORT = 2500, CLIENT_PORT = 2500 } = process.env;
 
@@ -12,9 +14,31 @@ new SafeUdpReceiver({
   serverPort: PORT,
 });
 
-const image = fs.readFileSync("src/images/10MBImage.jpg");
 
-fs.unlinkSync("combined.log");
-fs.unlinkSync("error.log");
+ const readImage = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "Digite o caminho da imagem",
+  },
+]; 
+
+await inquirer.prompt(readImage).then(answers => {
+  console.log(`caminho digitado ${answers.name}!`);
+  process.argv[2] = answers.name
+}); 
+
+
+const image = process.argv[2]
+
+console.log("Valor de image" + image)
+
+
+try {
+  fs.unlinkSync("combined.log");
+  fs.unlinkSync("error.log");
+} catch(error) {
+  console.log("Erro")
+}
 
 server.sendFile(image, CLIENT_PORT);
--- src/utils/files.js
+++ src/utils/files.js
@@ -1,11 +1,15 @@
-export const breakFileIntoChunks = (file, chunkSize) => {
+ export const breakFileIntoChunks = (file, chunkSize) => {
   const bufferChunks = [];
 
+  try {
   for (let i = 0; i < file.length; i += chunkSize) {
     const content = file.subarray(i, i + chunkSize);
 
     bufferChunks.push(content);
-  }
+  } 
+} catch(error) {
+  console.log("erro aqui tmb")
+}
 
   return bufferChunks;
-};
+}; 
